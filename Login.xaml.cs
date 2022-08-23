00822b6a8eab505b2682768df431a7788009f75d
iiiiiillllllllllllllllllll <102303737+iiiiiillllllllllllllllllll@users.noreply.github.com>
2022-08-23 14:20:57 -0400

    Att

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ProjetoLuna
 {
     /// <summary>
-    /// Interação lógica para MainWindow.xam
+    /// Lógica interna para Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
