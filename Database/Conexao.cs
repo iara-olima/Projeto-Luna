952f2c9027c110c0ae438dd4237b721e4b14f9a2
iiiiiillllllllllllllllllll <102303737+iiiiiillllllllllllllllllll@users.noreply.github.com>
2022-08-29 23:51:02 -0400

    Conexao

---
+++ Database/Conexao.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLuna.Database
+{
+    internal class Conexao
+    {
+        private static string host = "localhost";
+
+        private static string port = "3306";
+
+        private static string user = "root";
+
+        private static string password = "aniversario01";
+
+        private static string dbname = "bd_escola";
+
+        private static MySqlConnection connection;
+
+        private static MySqlCommand command;
+
+        public Conexao()
+        {
+            try
+            {
+                connection = new MySqlConnection($"server={host};database={dbname};port={port};user={user};password={password}");
+                connection.Open();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public MySqlCommand Query()
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+
+                return command;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
