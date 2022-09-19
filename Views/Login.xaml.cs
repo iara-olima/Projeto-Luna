9805550dd3afb427aa1f00843f8668e7c7cc2dca
Isabelola <96275763+Isabelola@users.noreply.github.com>
2022-09-19 14:03:50 -0400

    material

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -62,5 +62,10 @@ namespace ProjetoLuna.Views
         {
             txtCPF.SelectAll();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
