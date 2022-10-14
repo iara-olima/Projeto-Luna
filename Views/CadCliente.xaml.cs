4a5cc35a7f299ddc4d1f408f7b3f6c1e4b46d8cd
iiiiiillllllllllllllllllll <102303737+iiiiiillllllllllllllllllll@users.noreply.github.com>
2022-10-14 13:19:12 -0400

    .

--- Views/CadCliente.xaml.cs
+++ Views/CadCliente.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ProjetoLuna.Models;
+using MySql.Data.MySqlClient;
 
 namespace ProjetoLuna.Views
 {
@@ -19,6 +21,8 @@ namespace ProjetoLuna.Views
     /// </summary>
     public partial class CadCliente : Window
     {
+        public Cliente cliente = new Cliente();
+
         public CadCliente()
         {
             InitializeComponent();
@@ -26,6 +30,52 @@ namespace ProjetoLuna.Views
 
         private void btSalvar_Click(object sender, RoutedEventArgs e)
         {
+
+            cliente.Nome = txtNome.Text;
+            cliente.Email = txtEmail.Text;
+            cliente.Telefone = txtTelefone.Text;
+            cliente.CPF = txtCpf.Text;
+            cliente.Endereco = txtEndereco.Text;
+            cliente.Sexo = cbSexo.Text;
+
+            if (dtNasc.SelectedDate != null)
+                cliente.DataNasc = dtNasc.SelectedDate;
+            ShowMenssage();
+
+            try
+            {
+                var dao = new ClienteDAO();
+                if (cliente.Id > 0)
+                {
+                    dao.Update(cliente);
+                    MessageBox.Show("Dados do Cliente atualizados com sucesso!");
+
+                }
+                else
+                {
+                    dao.Insert(cliente);
+                    MessageBox.Show("Cliente cadastrado com sucesso.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ShowMenssage()
+        {
+
+            MessageBox.Show("Nome: " + cliente.Nome + Environment.NewLine +
+                 "razão social= " + _escola.RazaoSocial + Environment.NewLine + "cnpj= " +
+                             _escola.CNPJ + Environment.NewLine + "inscrição estadual= " + _escola.Inscricao + Environment.NewLine + "responsavel= " + _escola.Responsavel +
+                             Environment.NewLine + "tipo= " + tipo + Environment.NewLine + Environment.NewLine + "data criação= " + _escola.DataCriacao +
+                             Environment.NewLine + "responsavel= " + _escola.Responsavel + Environment.NewLine + "telefone resp= " + _escola.TelefoneResp + Environment.NewLine +
+                             "e-mail= " + _escola.Email + Environment.NewLine + "telefone esc= " + _escola.TelefoneEsc + Environment.NewLine + " rua= " + _escola.Rua + Environment.NewLine +
+                           "numero= " + _escola.Numero + Environment.NewLine + "bairro= " + _escola.Bairro + Environment.NewLine + "complemento= " + _escola.Complemento + Environment.NewLine +
+                          "cep= " + _escola.CEP + Environment.NewLine + "cidade= " + _escola.Cidade + Environment.NewLine + "estado= " + _escola.Estado + Environment.NewLine
+                             );
+
 
         }
 
