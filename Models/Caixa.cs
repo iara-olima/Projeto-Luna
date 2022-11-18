37ef0c94b7672e593bd3c02e16b9d6a657c49a6e
Rafael Angelo Darold <100599507+RafaelDarold@users.noreply.github.com>
2022-11-18 08:48:42 -0400

    Revert "Atualização em CaixaDAO (porque tinha simplesmente sumido kkk)."
    This reverts commit 20660ebdac7bdfff1680ac37f0c06557d8bb1276.

--- Models/Caixa.cs
+++ Models/Caixa.cs
@@ -8,7 +8,7 @@ namespace ProjetoLuna.Models
 {
     public class Caixa
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public DateTime? Data { get; set; }
         public Double? SaldoInicial { get; set; }
         public Double? SaldoFinal { get; set; }
