06f3cd2a60b9ced60620b0bd5d1071cc62f55a3c
Rahyan Ramos <rahyanramos@gmail.com>
2023-02-02 16:50:59 -0400

    Organizando alterações

---
+++ Models/ProdutoVenda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLuna.Models
+{
+    public class ProdutoVenda
+    {
+        public int Id { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; set; }
+        public int IdVenda { get; set; }
+        public int IdProduto { get; set; }
+    }
+}
